d702dba492e7c99c4197f23f359200232ecba8f8
Sravanthi Murukonda <113963634+sravanthimurukonda@users.noreply.github.com>
2024-09-22 18:58:06 +0530

    Update DataInput.js

--- src/DataInput.js
+++ src/DataInput.js
@@ -23,7 +23,7 @@ const DataInput = () => {
         setError('');
         try {
             const jsonData = JSON.parse(inputData);
-            const response = await axios.post('https://bhfl-backend-cabm.onrender.com/bfhl', jsonData);
+            const response = await axios.post('https://bajaj-backend-tgz9.onrender.com/bfhl', jsonData);
             setResponseData(response.data);
         } catch (err) {
             console.error('Submission error:', err);
